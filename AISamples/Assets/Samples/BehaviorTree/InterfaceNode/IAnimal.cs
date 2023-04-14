2852b12809b03f4458702814c47da8b74a7c3ca4
云却 <479813005@qq.com>
2023-04-14 19:52:55 +0800

    接口示例

---
+++ AISamples/Assets/Samples/BehaviorTree/InterfaceNode/IAnimal.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Megumin.GameFramework.AI.BehaviorTree;
+using Megumin.GameFramework.AI;
+
+public interface IAnimal
+{
+    void Wow();
+}
+
+public class AnimalWow : BTActionNode<IAnimal>
+{
+    protected override Status OnTick(BTNode from)
+    {
+        MyAgent.Wow();
+        return Status.Succeeded;
+    }
+}
+
