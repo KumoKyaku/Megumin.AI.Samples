d06e1bf0ef192580ce94a0f57e2c9c3802441f5f
云却 <479813005@qq.com>
2023-10-06 14:17:49 +0800

    增加MyRefVar示例

---
+++ AISamples/Assets/Samples/BehaviorTree/MyRefVar/MyRefVarTest.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using Megumin.Binding;
+using UnityEditor;
+using UnityEngine;
+
+namespace Megumin.AI.BehaviorTree
+{
+    [Serializable]
+    public class MyTestVarData
+    {
+        public int a = 0;
+    }
+
+    [Serializable]
+    [DebuggerTypeProxy(typeof(DebugView))]
+    public class RefVar_MyTestVarData : RefVar<MyTestVarData>
+    {
+
+    }
+
+    public class VariableCreator_MyTestVarData : VariableCreator
+    {
+        public override string Name { get; set; } = "MyTest/Date";
+
+        public override IRefable Create()
+        {
+            return new RefVar_MyTestVarData() { RefName = "MyTestVarData" };
+        }
+
+
+#if UNITY_EDITOR
+        [UnityEditor.InitializeOnLoadMethod]
+#endif
+        //[UnityEngine.RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+        static void AddToAllCreator()
+        {
+            VariableCreator.AllCreator.Add(new VariableCreator_MyTestVarData());
+            //Or insert.
+            //VariableCreator.AllCreator.Insert(0,new VariableCreator_MyTestVarData());
+        }
+    }
+}
+
+
+
+
+
